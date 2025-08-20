b779efcbcbb62846d5dde6fad7fcd22a0271a158
Nilesh Raju Ambekar <nileshambekar9281@gmail.com>
2025-08-20 17:28:07 +0530

    minor change

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -13,11 +13,11 @@ const About = () => {
           </p>
           
           <p className="about-text">
-            I speak multiple languages - Java, JavaScript, TypeScript, C/C++ - and no, I don't mean at parties (though that would be impressive). My superpower? Turning coffee into code and complex problems into elegant solutions.
+            I speak multiple languages - Java, JavaScript, TypeScript, C/C++ - and no, I don't mean at parties (though that would be impressive). My superpower? Turning <span class="tilted-strike">coffee</span> Chai into code and complex problems into elegant solutions.
           </p>
           
           <p className="about-text">
-            In my free time, you'd find me listening to music or touching grass (yes, I do go outside occasionally). When I'm not debugging code, I'm debugging my life, one cup of coffee at a time.
+            In my free time, you'd find me listening to music or touching grass (yes, I do go outside occasionally). When I'm not debugging code, I'm debugging my life, one cup of <span class="tilted-strike">Coffee</span> Chai at a time.
           </p>
         </div>
       </div>
